33baeb9b0fdd1bf19f5fc9b540e3d792b2d067ae
Iryna1791 <irynawebdev@gmail.com>
2023-01-04 21:43:18 +0100

    done

---
+++ rooms-app/routes/auth.routes.js
@@ -0,0 +1,88 @@
+const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const saltRounds = 10;
+
+const User = require('../models/User.model');
+
+/* GET Signup page */
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", async (req,res) => {
+    const { fullName, password, email } = req.body;
+ 
+    // make sure users fill all mandatory fields:
+    if (!fullName || !password || !email) {
+      res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+      return;
+    }
+
+    // make sure passwords are strong:
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+  
+  User.create({fullName, email, password: passwordHash})
+      .then((newUser) => {
+      res.redirect(`/auth/login`)
+      })
+      .catch(err => console.log(err))
+})
+
+router.get('/login', (req, res) => {
+    
+    res.render('auth/login')
+})
+
+router.post('/login', (req, res) => {
+    
+    const { password, email } = req.body;
+    console.log('req.body', req.body)
+//    Data validation check 
+  if (email === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+
+  User.findOne({ email })
+    .then(user => { // --> { username: '', email: '', password: ''} || null
+        console.log('user', user)
+      if (!user) { // if user is not found in the DB
+        res.render('auth/login', { errorMessage: 'Username is not registered. Try with other email.' });
+        return;
+      } else if (bcrypt.compareSync(password, user.password)) { // if password is correct
+        // res.redirect(`/auth/profile/${user.username}`)
+        // res.render('auth/profile', user);
+        
+        req.session.currentUser =  user ; // creating the property currentUser 
+        console.log( "the req.sessio",req.session.currentUser)
+        res.redirect('/auth/profile')
+        
+      } else { // if password is incorect
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => console.log(error));
+})
+
+router.get('/profile', (req, res) => {
+    console.log(req.session.currentUser)
+    res.render('auth/profile', req.session.currentUser)
+})
+
+router.post('/logout', (req, res) => {
+    req.session.destroy(err => {
+      if (err) console.log(err);
+      res.redirect('/');
+    });
+  });
+
+module.exports = router;
---
+++ rooms-app/routes/room.routes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+
+const isLoggedIn = require("../middleware/isLoggedIn")
+
+// require the Room model here
+const Room = require('../models/Room.model');
+     //rooms
+router.get('/', (req, res, next) => {
+  // Iteration #2: List the room
+  Room.find()
+      .then(rooms => res.render('rooms/list', { rooms }))
+      .catch(err => console.log(err))
+});
+
+router.get('/create', isLoggedIn, (req, res, next) => {
+  // Add a new room
+  console.log("when you click on create room",req.session.currentUser)
+  res.render('rooms/create-form')
+});
+
+router.post('/create', (req, res, next) => {
+  // Iteration #3: Add a new room
+  const {name, description, imageUrl} = req.body;
+
+  Room.create({
+    name,
+    description,
+    imageUrl,
+    owner : req.session.currentUser._id
+   
+
+  })
+  .then(()=> res.redirect('/rooms'))
+  .catch(err => console.log(err))
+});
+
+router.get('/:id/edit', (req, res, next) => {
+  // Iteration #4: Update the rooms
+
+  console.log("req.params",req.params)
+  const { id } = req.params;
+
+
+  Room.findById(id)
+    .then(foundRoom => res.render('rooms/update-form', foundRoom))
+    .catch(err => console.log(err))
+  
+});
+
+router.post('/:id/edit', (req, res, next) => {
+  // Iteration #4: Update the rooms
+  const { name, description, imageUrl } = req.body;
+  const { id } = req.params;
+
+  Room.findByIdAndUpdate(id, {name, description, imageUrl})
+      .then(() => res.redirect('/rooms'))
+      .catch(err => console.log(err))
+  
+});
+
+router.post('/:id/delete', (req, res, next) => {
+  // Iteration #5: Delete the room
+  const { id } = req.params;
+
+  Room.findByIdAndDelete(id)
+      .then(() => res.redirect('/rooms'))
+      .catch(err => console.log(err))
+});
+
+module.exports = router;
